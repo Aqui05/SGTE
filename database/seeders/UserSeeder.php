7aa71d13d963cf3454b5caae50e63c5389af0e94
KIKISSAGBE Aquilas <kikissagbeaquilas@gmail.com>
2024-10-30 16:17:17 +0100

    modified:   .env.example 	modified:   .gitignore 	modified:   README.md 	modified:   composer.lock 	modified:   database/seeders/UserSeeder.php

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,8 +14,35 @@ class UserSeeder extends Seeder
     /**
      * Run the database seeds.
      */
-    public function run(): void
+     public function run(): void
     {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'remember_token' => Str::random(10),
+            'role' => 'admin',
+            'phone' => fake()->phoneNumber(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode(),
+            'country' => fake()->countryCode(),
+        ]);
+
+
+        User::create([
+            'name' => 'User',
+            'email' => 'user@gmail.com',
+            'email_verified_at' => now(),
+            'password' => Hash::make('123456'),
+            'remember_token' => Str::random(10),
+            'role' => 'user',
+            'phone' => fake()->phoneNumber(),
+            'city' => fake()->city(),
+            'zip' => fake()->postcode(),
+            'country' => fake()->countryCode(),
+        ]);
+
         User::factory(50)->create();
     }
 }
